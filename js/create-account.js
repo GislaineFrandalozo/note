1b2342b3e306147a9698dea131200acf2854518d
Gislaine Frandalozo <frandalozo.gislaine@gmail.com>
2021-09-08 01:31:58 -0400

    create-account

---
+++ js/create-account.js
@@ -0,0 +1,46 @@
+const input_name = document.querySelector("#name")
+const input_date = document.querySelector("#date")
+const input_email = document.querySelector("#email")
+const input_password = document.querySelector("#password")
+const button = document.querySelector("#button")
+
+const crateAccount = button.addEventListener('click', () => {
+
+    try{
+        validateInputs(input_name.value, input_password.value)
+        const user = new form (input_name.value,input_date.value, input_email.value, input_password.value)
+    
+        const userAccount = localStorage.getItem(input_name.value)
+        if(userAccount){ 
+        throw "Este nome já está sendo usado"
+        }else{ 
+        localStorage.setItem(input_name.value, JSON.stringify(user))
+        console.log(localStorage)
+        alert("Cadastro criado com sucesso, faça login para iniciar a sessão!")
+        }
+
+    }catch(e){
+        alert(e)
+    }
+
+})
+
+class form {
+    constructor(name, date, email, password){
+        this.name = name
+        this.date = date
+        this.email = email
+        this.password = password
+    }
+}
+function validateInputs(name, password) {
+    if(name.length < 3){
+        alert("Nome muito curto, no mínimo três caracteres.")
+    }
+    if (password.length < 3){
+        alert("Senha muito curta, no mínimo três caractres.")
+    }
+    else{
+        return console.log("")
+    }
+}
