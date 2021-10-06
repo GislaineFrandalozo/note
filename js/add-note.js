341713aa7aacd57c0c6c8436984fbb23f565ab12
Gislaine <frandalozo.gislaine@gmail.com>
2021-10-06 01:41:07 -0400

    settings

--- js/add-note.js
+++ js/add-note.js
@@ -1,8 +1,59 @@
 import { session } from './session.js'
 
-const newSession = session.verifySession("html/add-note.html")
+const newSession = session.verifySession("html/home-note.html")
 
-const button = document.querySelector("#sign_out")
-button.addEventListener('click', () => {
-    const exit = session.sign_outSession("html/add-note.html")
+/* Settings */
+const settings = document.querySelector("#settings")
+settings.addEventListener('click', () => {
+    const x = new Promise((resolve, reject) => {
+        const control = document.querySelector('.options')
+        if (control) {
+            reject(control)
+        } else {
+            const element = document.querySelector(".settings")
+            const div = document.createElement("div")
+            element.appendChild(div)
+            div.setAttribute('class', 'options')
+            div.innerHTML = '<button id="sign_out">SAIR</button>'
+            resolve()
+        }
+    })
+    x.then(() => {
+        const button = document.querySelector("#sign_out")
+        button.addEventListener('click', () => {
+            const exit = session.sign_outSession("html/home-note.html")
+        })
+    }).catch((control) => {
+        control.remove()
+    })
+
+})
+
+/* Menu */
+const colorNote = document.querySelector(".color")
+const note = document.querySelector(".note")
+note.style.backgroundColor = colorNote.value
+colorNote.addEventListener('change', (e) => {
+    note.style.backgroundColor = e.target.value
+})
+
+const text = document.querySelector("#text")
+text.addEventListener('click', () => {
+    alert("Desculpa ainda não desenvolvemos esta operação :c")
+})
+const checklist = document.querySelector("#checklist")
+checklist.addEventListener('click', () => {
+    alert("Desculpa ainda não desenvolvemos esta operação :c")
+})
+const table = document.querySelector("#table")
+table.addEventListener('click', () => {
+    alert("Desculpa ainda não desenvolvemos esta operação :c")
+})
+const link = document.querySelector("#link")
+link.addEventListener('click', () => {
+    alert("Desculpa ainda não desenvolvemos esta operação :c")
+})
+const calculator = document.querySelector("#calculator")
+calculator.addEventListener('click', () => {
+    alert("Desculpa ainda não desenvolvemos esta operação :c")
 })
--- js/home.js
+++ js/home.js
@@ -2,7 +2,29 @@ import { session } from './session.js'
 
 const newSession = session.verifySession("html/home-note.html")
 
-const button = document.querySelector("#sign_out")
-button.addEventListener('click', () => {
-    const exit = session.sign_outSession("html/home-note.html")
+/* Settings */
+const settings = document.querySelector("#settings")
+settings.addEventListener('click', () => {
+    const x = new Promise((resolve, reject) => {
+        const control = document.querySelector('.options')
+        if (control) {
+            reject(control)
+        } else {
+            const element = document.querySelector(".settings")
+            const div = document.createElement("div")
+            element.appendChild(div)
+            div.setAttribute('class', 'options')
+            div.innerHTML = '<button id="sign_out">SAIR</button>'
+            resolve()
+        }
+    })
+    x.then(() => {
+        const button = document.querySelector("#sign_out")
+        button.addEventListener('click', () => {
+            const exit = session.sign_outSession("html/home-note.html")
+        })
+    }).catch((control) => {
+        control.remove()
+    })
+
 })
